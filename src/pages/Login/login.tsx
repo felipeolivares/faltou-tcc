971f85b255e560501625e8c7fb9f959350918c6b
Felipe Olivares Campos <felipeolivares99@hotmail.com>
2023-01-27 19:16:38 -0300

    new login and register pages

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,69 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
+import * as Yup from "yup";
 import { Box, Paper } from "@material-ui/core";
 import { TextField, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import routes from "routes/routes";
+import logo from "assets/Logo.png";
 
 import useStyles from "../styles";
+import { useFormik } from "formik";
 
 const Login: React.FC = () => {
   const classes = useStyles();
+  const navigate = useNavigate();
+  const appValues = {
+    email: "",
+    password: "",
+  };
+
+  const formik = useFormik({
+    validationSchema: Yup.object().shape({
+      email: Yup.string().required("Campo Obrigatório"),
+      password: Yup.string().required("Campo Obrigatório"),
+    }),
+    initialValues: appValues,
+    validateOnChange: false,
+
+    onSubmit: (values: any) => {
+      // updateAppValues(values);
+      // setPreviousPage(routes.searchDentist);
+      // setPage(routes.resultSearchDentist);
+      // navigate(routes.resultSearchDentist);
+      console.log("teste");
+    },
+  });
+
+  const onClickValue = () => {
+    navigate(routes.register);
+  };
 
   return (
-    <Box className={classes.containerLogin}>
-      <Paper elevation={3} className={classes.loginPage}>
-        <Box className={classes.loginMsg}>
-          <Typography variant="h4" fontWeight="bold">
-            Login
-          </Typography>
-        </Box>
-        <Box className="pt16">
-          <TextField
-            id="dentistName"
-            name="dentistName"
-            // value={formik.values.dentistName}
-            // onChange={formik.handleChange}
-            // onBlur={formik.handleBlur}
-            label="Informe seu login"
-            placeholder="Informe seu login"
-            autoComplete="off"
-            fullWidth
-          />
-        </Box>
-        <Box className="pt10">
-          <TextField
-            id="dentistName"
-            name="dentistName"
-            // value={formik.values.dentistName}
-            // onChange={formik.handleChange}
-            // onBlur={formik.handleBlur}
-            label="Informe sua senha"
-            placeholder="Informe sua senha"
-            autoComplete="off"
-            fullWidth
-          />
-        </Box>
-        <Box className="pt10">
-          <Button
-            id="searchbtn"
-            color="primary"
-            variant="contained"
-            className="btn-height"
-            onClick={() => console.log("teste")}
-            type="submit"
-            fullWidth
-          >
-            <Typography variant="caption" fontWeight="bold">
-              Entrar
+    <form onSubmit={formik.handleSubmit}>
+      <Box className={classes.containerLogin}>
+        <Paper elevation={3} className={classes.loginPage}>
+          <Box className={classes.loginMsg}>
+            <img className={classes.containerImage} alt="iconPlus" src={logo} />
+            <Typography variant="h4" fontWeight="bold">
+              Login
             </Typography>
-          </Button>
-        </Box>
-        <Box className={classes.register}>
-          <Typography fontSize="12px">Não é registrado?</Typography>
-          <Typography fontSize="12px" className={classes.createAccount}>
-            Crie uma conta.
-          </Typography>
-        </Box>
-      </Paper>
-    </Box>
+          </Box>
+          <Box className="pt16">
+            <TextField
+              id="email"
+              name="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={!!formik.touched.email && !!formik.errors.email}
+              label="Informe seu e-mail"
+              placeholder="Informe seu e-mail"
+              autoComplete="off"
+              fullWidth
+            />
+          </Box>
+          <Box className="pt12">
+            <TextField
+              id="password"
+              name="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={!!formik.touched.password && !!formik.errors.password}
+              label="Informe sua senha"
+              placeholder="Informe sua senha"
+              autoComplete="off"
+              fullWidth
+            />
+          </Box>
+          <Box className="pt12">
+            <Button
+              id="enterbtn"
+              color="primary"
+              variant="contained"
+              className="btn-height"
+              onClick={() => console.log("teste")}
+              type="submit"
+              fullWidth
+            >
+              <Typography variant="caption" fontWeight="bold">
+                Entrar
+              </Typography>
+            </Button>
+          </Box>
+          <Box className={classes.register}>
+            <Typography fontSize="12px">Não é registrado?</Typography>
+            <Typography
+              fontSize="12px"
+              className={classes.createAccount}
+              onClick={onClickValue}
+              id="registerClick"
+            >
+              Crie uma conta.
+            </Typography>
+          </Box>
+        </Paper>
+      </Box>
+    </form>
   );
 };
 
--- src/pages/Register/register.tsx
+++ src/pages/Register/register.tsx
@@ -1,69 +1,132 @@
 import React from "react";
+import * as Yup from "yup";
 import { Box, Paper } from "@material-ui/core";
 import { TextField, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import routes from "routes/routes";
+import logo from "assets/Logo.png";
+import backIcon from "assets/BackIcon.png";
+import { useFormik } from "formik";
 
 import useStyles from "../styles";
 
 const Register: React.FC = () => {
   const classes = useStyles();
+  const navigate = useNavigate();
+  const appValues = {
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const formik = useFormik({
+    validationSchema: Yup.object().shape({
+      email: Yup.string().required("Campo Obrigatório"),
+      password: Yup.string().required("Campo Obrigatório"),
+      confirmPassword: Yup.string().required("Campo Obrigatório"),
+    }),
+    initialValues: appValues,
+    validateOnChange: false,
+
+    onSubmit: (values: any) => {
+      // updateAppValues(values);
+      // setPreviousPage(routes.searchDentist);
+      // setPage(routes.resultSearchDentist);
+      // navigate(routes.resultSearchDentist);
+      console.log("teste");
+    },
+  });
+
+  const onClickValue = () => {
+    navigate(routes.login);
+  };
 
   return (
-    <Box className={classes.containerLogin}>
-      <Paper elevation={3} className={classes.loginPage}>
-        <Box className={classes.loginMsg}>
-          <Typography variant="h4" fontWeight="bold">
-            Login
-          </Typography>
-        </Box>
-        <Box className="pt16">
-          <TextField
-            id="dentistName"
-            name="dentistName"
-            // value={formik.values.dentistName}
-            // onChange={formik.handleChange}
-            // onBlur={formik.handleBlur}
-            label="Informe seu login"
-            placeholder="Informe seu login"
-            autoComplete="off"
-            fullWidth
-          />
-        </Box>
-        <Box className="pt10">
-          <TextField
-            id="dentistName"
-            name="dentistName"
-            // value={formik.values.dentistName}
-            // onChange={formik.handleChange}
-            // onBlur={formik.handleBlur}
-            label="Informe sua senha"
-            placeholder="Informe sua senha"
-            autoComplete="off"
-            fullWidth
-          />
-        </Box>
-        <Box className="pt10">
-          <Button
-            id="searchbtn"
-            color="primary"
-            variant="contained"
-            className="btn-height"
-            onClick={() => console.log("teste")}
-            type="submit"
-            fullWidth
-          >
-            <Typography variant="caption" fontWeight="bold">
-              Entrar
+    <form onSubmit={formik.handleSubmit}>
+      <Box className={classes.containerLogin}>
+        <Paper elevation={3} className={classes.loginPage}>
+          <Box className={classes.loginMsg}>
+            <img className={classes.containerImage} alt="iconPlus" src={logo} />
+            <Typography variant="h4" fontWeight="bold">
+              Cadastro
             </Typography>
-          </Button>
-        </Box>
-        <Box className={classes.register}>
-          <Typography fontSize="12px">Não é registrado?</Typography>
-          <Typography fontSize="12px" className={classes.createAccount}>
-            Crie uma conta.
-          </Typography>
-        </Box>
-      </Paper>
-    </Box>
+          </Box>
+          <Box className="pt16">
+            <TextField
+              id="email"
+              name="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={!!formik.touched.email && !!formik.errors.email}
+              label="Informe seu e-mail"
+              placeholder="Informe seu e-mail"
+              autoComplete="off"
+              fullWidth
+            />
+          </Box>
+          <Box className="pt12">
+            <TextField
+              id="password"
+              name="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={!!formik.touched.password && !!formik.errors.password}
+              label="Informe sua senha"
+              placeholder="Informe sua senha"
+              autoComplete="off"
+              fullWidth
+            />
+          </Box>
+          <Box className="pt12">
+            <TextField
+              id="confirmPassword"
+              name="confirmPassword"
+              value={formik.values.confirmPassword}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={
+                !!formik.touched.confirmPassword &&
+                !!formik.errors.confirmPassword
+              }
+              label="Confirme sua senha"
+              placeholder="Confirme sua senha"
+              autoComplete="off"
+              fullWidth
+            />
+          </Box>
+          <Box className="pt12">
+            <Button
+              id="searchbtn"
+              color="primary"
+              variant="contained"
+              className="btn-height"
+              onClick={() => console.log("teste")}
+              type="submit"
+              fullWidth
+            >
+              <Typography variant="caption" fontWeight="bold">
+                Cadastrar
+              </Typography>
+            </Button>
+          </Box>
+          <Box className={classes.containerBack} onClick={() => onClickValue()}>
+            <Box className={classes.imageBackIcon}>
+              <img alt="backIcon" src={backIcon} />
+            </Box>
+            <Typography
+              fontSize="12px"
+              fontWeight="bold"
+              color="#0061A0"
+              paddingTop={"3px"}
+            >
+              VOLTAR
+            </Typography>
+          </Box>
+        </Paper>
+      </Box>
+    </form>
   );
 };
 
